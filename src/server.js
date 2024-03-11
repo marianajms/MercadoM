6c5a27e81652dd926b6ab73f6c578341a3343170
Mariana Jager <mariana.macedosoares@hotmail.com>
2024-03-10 21:37:32 -0300

    construção de servidor com express, header e primeiro card

---
+++ src/server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+
+const app = express();
+const porta = 3000;
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', (req,res)=>{
+    res.render('index.ejs')
+})
+
+app.get('/localizacao', (req,res)=>{
+  res.render('localizacao.ejs')
+})
+
+app.listen(porta, (error) => {
+    if (error) {
+      console.log("Ocorreu um erro");
+    } else {
+      console.log(`Servidor ativado na porta ${porta}`);
+    }
+  });
